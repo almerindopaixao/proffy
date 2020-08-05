c07ce0d7efad27e5807d87adfc75412276c654e7
Almerindo Paixão <almerindopaixao@gmail.com>
2020-08-05 10:07:51 -0300

    :books: Update Input Components

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -2,10 +2,34 @@ import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
 
+import './styles.css';
+import Input from '../../components/Input';
+
 export default function TeacherForm() {
   return (
     <div id="page-teacher-form" className="container">
-      <PageHeader title="Que incrivel que você quer dar aulas." />
+      <PageHeader 
+        title="Que incrivel que você quer dar aulas."
+        description="O primeiro passo é preencher esse formulário de descrição."  
+      />
+
+      <main>
+        <fieldset>
+          <legend>Seus dados</legend>
+
+          <Input name="name" label="Nome completo"/>
+          <Input name="avatar" label="Avatar"/>
+          <Input name="whatsapp" label="Whatsapp"/>
+        </fieldset>
+
+        <fieldset>
+          <legend>Sobre a aula</legend>
+
+          <Input name="name" label="Nome completo"/>
+          <Input name="avatar" label="Avatar"/>
+          <Input name="whatsapp" label="Whatsapp"/>
+        </fieldset>
+      </main>
     </div>
   );
 }
