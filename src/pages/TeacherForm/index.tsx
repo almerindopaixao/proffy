41f3e88e860dec0aff7306ebeb313f21c0af8f8a
Almerindo Paixão <almerindopaixao@gmail.com>
2020-08-05 10:31:11 -0300

    :page_facing_up: Update Pages

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
+import Input from '../../components/Input';
+
+import warningIcon from '../../assets/images/icons/warning.svg';
 
 import './styles.css';
-import Input from '../../components/Input';
 
 export default function TeacherForm() {
   return (
@@ -25,10 +27,20 @@ export default function TeacherForm() {
         <fieldset>
           <legend>Sobre a aula</legend>
 
-          <Input name="name" label="Nome completo"/>
-          <Input name="avatar" label="Avatar"/>
-          <Input name="whatsapp" label="Whatsapp"/>
+          <Input name="subject" label="Matéria"/>
+          <Input name="cost" label="Custo da sua hora por aula"/>
         </fieldset>
+
+        <footer>
+          <p>
+            <img src={warningIcon} alt="Aviso importante"/>
+            Importante! <br />
+            Preencha todos os dados
+          </p>
+          <button type="button">
+            Salvar Cadastro
+          </button>
+        </footer>
       </main>
     </div>
   );
