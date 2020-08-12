4312690d23475428d26af9bd9eccf986475674e3
Almerindo Paixão <almerindopaixao@gmail.com>
2020-08-12 20:20:30 -0300

    Update finnally week

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -3,34 +3,57 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
+import api from '../../services/api';
 
-export default function TeacherItem(){
+export interface Teacher {
+  id: number;
+  subject: string;
+  cost: number;
+  name: string,
+  avatar: string;
+  whatsapp: string;
+  bio: string;
+}
+
+interface TeacherItemProps {
+  teacher: Teacher; 
+}
+
+const TeacherItem: React.FunctionComponent<TeacherItemProps> = ({ teacher }) => {
+  
+  function createNewConnection() {
+    api.post('/connections', {
+      user_id: teacher.id,
+    });
+  }
   return (
     <article className="teacher-item">
     <header>
-      <img src="https://avatars3.githubusercontent.com/u/58123442?s=460&u=9a2176a2f67ba7d79a3d5f59069c7d655b2f016b&v=4" alt="Almerindo Paixão"/>
+      <img src={teacher.avatar} alt={teacher.name} />
       <div>
-        <strong>Almerindo Paixão</strong>
-        <span>Programação</span>
+        <strong>{teacher.name}</strong>
+        <span>{teacher.subject}</span>
       </div>
     </header>
 
-      <p>
-        Programadaor  front end
-        <br></br>
-        React, html, css, node, javascript
-      </p>
+    <p>{teacher.bio}</p>
 
       <footer>
         <p>
           Preço/hora
-          <strong>R$ 20,00</strong>
+          <strong>R$ {teacher.cost},00</strong>
         </p>
-        <button type="button">
+        <a 
+          target="_blank" 
+          onClick={createNewConnection} 
+          href={`https://wa.me/${teacher.whatsapp}`}
+        >
           <img src={whatsappIcon} alt="whatsapp"/>
           Entrar em contato
-        </button>
+        </a>
       </footer>
   </article>
   );
 } 
+
+export default TeacherItem;
